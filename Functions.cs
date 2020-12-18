4f8d563c6cda7a4fdcc5e61a4e34fba2f6e04c02
doris <doris@hanxia.onmicrosoft.com>
2020-12-18 15:19:59 +0800

    first commit

---
+++ Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TimerWebJob2
+{
+    public class Functions
+    {
+        public static void ProcessTimerTrigger([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger logger)
+        {
+            logger.LogInformation("Message from Class1.");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace TimerWebJob
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+                b.AddTimers();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
